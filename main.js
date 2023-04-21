04dc61c6d39a42810e967a582cdfdd4fc23a3007
nguyennguyenthanh <thanhnguyennguyen1228@gmail.com>
2023-04-21 18:29:23 +0700

    update submenu-header

--- main.js
+++ main.js
@@ -63,3 +63,14 @@ var white = document.getElementById("white").onclick = function() {
 };
 
 document.getElementById("white").style.display = "none";
+// -----DROPDOWN-MENU-----
+
+// Add slideDown animation to Bootstrap dropdown when expanding.
+$('.dropdown').on('show.bs.dropdown', function() {
+  $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+});
+
+// Add slideUp animation to Bootstrap dropdown when collapsing.
+$('.dropdown').on('hide.bs.dropdown', function() {
+  $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+});
